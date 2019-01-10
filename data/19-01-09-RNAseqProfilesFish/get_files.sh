6a3075563e6c21c997f54b334f5591f7cbeabe78
Santiago G. Medina <santiagogmm@hotmail.com>
2019-01-10 16:50:42 -0600

    EDA RNA-seq timecourse

--- data/19-01-09-RNAseqProfilesFish/get_files.sh
+++ data/19-01-09-RNAseqProfilesFish/get_files.sh
@@ -19,8 +19,8 @@ mv RSEM_TPM_table.csv rna-seq-profiles/polyA-profile.csv
 
 # a-aminitin profile
 wget http://bioinfo/n/core/Bioinformatics/secondary/Bazzini/arb/MOLNG-2539/secundo/RSEM_TPM_table.csv
-mv RSEM_TPM_table.csv rna-seq-profile/alpha-amanitin-prolife.csv
+mv RSEM_TPM_table.csv rna-seq-profiles/alpha-amanitin-prolife.csv
 
 # spike-ins counts
 wget http://bioinfo/n/core/Bioinformatics/secondary/Bazzini/arb/MOLNG-2539/secundo/ercc_count.csv
-mv ercc_count.csv alpha-amanitin-prolife.csv
+mv ercc_count.csv rna-seq-profiles
---
+++ results/19-01-10-EDA-RNA-time-course/01-EDA-time-course.md
@@ -0,0 +1,18 @@
+EDA RNA-seq time courses
+================
+Santiago Medina
+1/10/2019
+
+In this notebook I will visualize the time-course RNA-seq data.
+
+PCA analysis
+------------
+
+![](./figures/pca-1.png)
+
+The PCA analysis looks find!
+
+Bivariate distributions
+-----------------------
+
+![](./figures/bivariate_polyA-1.png)
