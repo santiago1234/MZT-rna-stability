89f738a0766d5f73211fdd5373c51291ef875daf
Santiago G. Medina <santiagogmm@hotmail.com>
2019-06-06 09:53:04 -0500

    eGFP optimization

---
+++ results/19-05-23-ProteinOptimization/eGFP.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+library(ggthemes)
+theme_set(theme_tufte(base_family = 'Helvetica'))
+
+
+eGFPseq <- "ATGGTGAGCAAGGGCGAGGAGCTGTTCACCGGGGTGGTGCCCATCCTGGTCGAGCTGGACGGCGACGTAAACGGCCACAAGTTCAGCGTGTCCGGCGAGGGCGAGGGCGATGCCACCTACGGCAAGCTGACCCTGAAGTTCATCTGCACCACCGGCAAGCTGCCCGTGCCCTGGCCCACCCTCGTGACCACCCTGACCTACGGCGTGCAGTGCTTCAGCCGCTACCCCGACCACATGAAGCAGCACGACTTCTTCAAGTCCGCCATGCCCGAAGGCTACGTCCAGGAGCGCACCATCTTCTTCAAGGACGACGGCAACTACAAGACCCGCGCCGAGGTGAAGTTCGAGGGCGACACCCTGGTGAACCGCATCGAGCTGAAGGGCATCGACTTCAAGGAGGACGGCAACATCCTGGGGCACAAGCTGGAGTACAACTACAACAGCCACAACGTCTATATCATGGCCGACAAGCAGAAGAACGGCATCAAGGTGAACTTCAAGATCCGCCACAACATCGAGGACGGCAGCGTGCAGCTCGCCGACCACTACCAGCAGAACACCCCCATCGGCGACGGCCCCGTGCTGCTGCCCGACAACCACTACCTGAGCACCCAGTCCGCCCTGAGCAAAGACCCCAACGAGAAGCGCGATCACATGGTCCTGCTGGAGTTCGTGACCGCCGCCGGGATCACTCTCGGCATGGACGAGCTGTACAAG"
+egfp_path <- read_csv("results_data/eGFP_optimization_path.csv") %>% 
+  select(-X1)
+
+
+egfp_path <- 
+  egfp_path %>% 
+  select(-fitness) %>% 
+  gather(key = specie, value = stability, -iteration, -sequences, -optimization)
+
+eGFPseq <- egfp_path %>% 
+  filter(sequences == eGFPseq, optimization == "maximization") %>% 
+  distinct()
+
+egfp_path %>%
+  ggplot(aes(x=iteration, y = stability)) +
+  geom_line(color="grey") +
+  geom_point(
+    data = eGFPseq,
+    color = "red"
+  ) +
+  ggrepel::geom_text_repel(
+    data = eGFPseq,
+    color = "red",
+        aes(label="eGFP")
+  ) +
+  facet_grid(~specie) +
+  geom_rangeframe()
+
+ggsave("figures/gfp_optimization.pdf", height = 3, width = 6)
+# print the best GFP ------------------------------------------------------
+
+cat(">eGFP\n")
+cat(eGFPseq$sequences[1])
+
+cat("\n>supremaGFP\n")
+egfp_path %>% 
+  filter(specie == "fish | embryo mzt | aamanitin polya") %>% 
+  arrange(-stability) %>% 
+  slice(1:1) %>% 
+  pull(sequences) %>% 
+  cat()
+
+cat("\n>infimaGFP\n")
+egfp_path %>% 
+  filter(specie == "fish | embryo mzt | aamanitin polya") %>% 
+  arrange(stability) %>% 
+  slice(1:1) %>% 
+  pull(sequences) %>% 
+  cat()
+cat("\n")
---
+++ results/19-05-23-ProteinOptimization/readme.md
@@ -0,0 +1,13 @@
+# optimizing eGFP
+
+1. Run the optimization
+
+```bash
+python optimiza_eGFP.py
+```
+
+2. Get the suprema and infima
+
+```bash
+Rscript eGFP.R >GFPsearch.txt
+```
