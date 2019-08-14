33b232d10c6b59bb097c74defc567fe830491641
Santiago G. Medina <santiagogmm@hotmail.com>
2019-08-14 12:41:00 -0500

    linear model fit to each specie

--- results/19-07-18-FeatureImportanceSpecies/03-plotResults.R
+++ results/19-07-18-FeatureImportanceSpecies/03-plotResults.R
@@ -6,40 +6,50 @@ theme_set(theme_tufte(base_family = "Helvetica"))
 dset <- read_csv("results_data/sample_data_for_feature_importance_analysis.csv")
 
 # fit a linear model to get the direction of effect
-
-directions <- lm(decay_rate ~ ., data = select(dset, -gene_id, -coding)) %>% 
-  broom::tidy() %>% 
-  filter(!str_detect(term, "specie"), !str_detect(term, "cell"), !str_detect(term, "data"), !str_detect(term, "Intercept")) %>% 
+# the model is fit to each specie
+directions <- 
+  select(dset, -gene_id, -coding, -datatype, -cell_type) %>% 
+  group_by(specie) %>% 
+  nest() %>% 
   mutate(
-    effect = sign(estimate)
+    fit = map(data, ~lm(decay_rate ~ ., data = .)),
+    tidy = map(fit, broom::tidy)
   ) %>% 
-  select(term, effect) %>% 
+  unnest(tidy) %>% 
+  mutate(effect = sign(estimate)) %>% 
+  filter(term != "(Intercept)") %>%
+  select(specie, term, effect) %>% 
   rename(feature = term)
 
 
-importance_all <- read_csv("results_data/feature_imp_all.csv") %>% 
-  inner_join(directions) %>% 
-  select(feature, effect, importance) %>% 
+
+importance_all <- read_csv("results_data/feature_imp_all.csv")
+
+## top 30 most important
+
+top_30_featutes <- arrange(importance_all, -importance) %>% 
+  filter(!feature %in% c("cell_type", "datatype", "specie")) %>% 
+  pull(feature) %>% 
+  .[1:30]
+  
+
+importance_all <- importance_all %>% 
+  select(feature, importance) %>% 
   rename(global_imp = importance)
 
 
-top_30_featutes <- arrange(importance_all, -global_imp) %>% 
-  pull(feature) %>% 
-  .[1:30]
 
 importance_specie <- read_csv("results_data/feature_imp_by_specie.csv") %>% 
+  inner_join(directions, by = c("feature", "specie")) %>% 
   inner_join(importance_all)
 
 
 importance_specie %>% 
   mutate(specie = factor(specie, levels = c("human", "mouse", "fish", "xenopus"))) %>% 
   filter(feature %in% top_30_featutes) %>% 
-  ggplot(aes(x=importance, y=reorder(feature, global_imp), color=specie)) +
-  geom_point(aes(shape = as.character(effect)), alpha=.9) +
-  geom_errorbarh(aes(xmin = importance.05, xmax = importance.95), alpha=1/4, height = 0) +
+  ggplot(aes(x=importance, y=reorder(feature, global_imp), color=as.character(effect))) +
+  geom_point(aes(shape = specie), alpha=.9) +
   scale_color_colorblind() +
-  scale_shape_manual("effect", values = c(16, 17)) +
-  geom_rangeframe(alpha=1/2, sides = "b") +
   labs(
     y = NULL,
     x = "Feature Importance (loss:mae)",
@@ -47,4 +57,4 @@ importance_specie %>%
   ) +
   theme(legend.position = "bottom")
 
-ggsave("feature_imp.pdf", height = 7, width = 5)
+ggsave("feature_imp.pdf", height = 5.5, width = 4.5)
