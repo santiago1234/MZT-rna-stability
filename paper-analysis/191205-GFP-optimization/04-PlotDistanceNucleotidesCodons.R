6785c9cd5527eb2c5c5308914d5e03ecb4d4b655
santiago1234 <santiagomm@hotmail.com>
2020-11-16 12:09:24 -0600

    pairwaise heatmap with optimalcodonR functions

--- paper-analysis/191205-GFP-optimization/04-PlotDistanceNucleotidesCodons.R
+++ paper-analysis/191205-GFP-optimization/04-PlotDistanceNucleotidesCodons.R
@@ -1,40 +1,15 @@
 library(tidyverse)
 library(Biostrings)
-
+library(optimalcodonR) # tienes que instalar esto: https://github.com/santiago1234/optimalcodonR
 
 # load the sequences and save them as fasta -------------------------------
 # I manually created the file adding/removing codon to make the neutralGFP match
 seqs <- readBStringSet("seqs-trimmed-for-counting-differences.csv") %>% 
   DNAStringSet()
 
-seqs
-s1 <- as.character(seqs)[3]
-s2 <- as.character(seqs)[2]
-
-
-get_nucs <- function(sequence) {
-  seq(from=1, to=str_length(sequence), by = 1) %>% 
-    map_chr(~str_sub(string = sequence, start = ., end = .))
-}
-
-get_codons <- function(sequence) {
-  seq(from=1, to=str_length(sequence)-3+1, by = 3) %>% 
-    map_chr(~str_sub(string = sequence, start = ., end = . + 2))
-}
-
-
-seq_distance <- function(s1, s2, iter_function) {
-  sum(iter_function(s1) != iter_function(s2))
-  
-}
-
-codon_distance
-seq_distance(s1, s2, get_codons)
-
-
 
 # create table with function ----------------------------------------------
-
+# la idea es tener una tabla con dos columnas: (nombre de secuencia) y (secuencia)
 x_1 <- tibble(
   id_1 = names(seqs),
   seq_1 = as.character(seqs)
@@ -45,16 +20,30 @@ x_2 <- tibble(
   seq_2 = as.character(seqs)
 )
 
+# aqui tengo una tabla con las combinaciones pareadas
 sequencias <- crossing(x_1, x_2)
 
+# uso dos funciones del paquete optimalcodonR
+# checa este ejemplo
+s1 <- sequencias$seq_1[1]
+s2 <- sequencias$seq_2[2]
+
+# cuantos nucleotidos son diferentes?
+nucleotide_distance(s1, s2)
+
+# cuantos codones son diferentes?
+codon_distance(s1, s2)
+
 ## compute the distance
+## unas funciones para correrlo en la tabla
 
 distancias <- 
   sequencias %>% 
   mutate(
-    dist_nucs = map2_dbl(seq_1, seq_2, function(x, y) seq_distance(x, y, get_nucs)),
-    dist_codons = map2_dbl(seq_1, seq_2, function(x, y) seq_distance(x, y, get_codons))
+    dist_nucs = map2_dbl(seq_1, seq_2, function(x, y) nucleotide_distance(x, y)),
+    dist_codons = map2_dbl(seq_1, seq_2, function(x, y) codon_distance(x, y))
   )
+
 
 # grafico -----------------------------------------------------------------
 
@@ -68,7 +57,7 @@ distancias %>%
   geom_tile() +
   geom_text(aes(label=labeler), color="grey90") +
   scale_y_discrete(expand = c(0, 0)) +
-  ggthemes::theme_tufte() +
+  theme_minimal() +
   theme(legend.position = "none")
 
 ggsave("distance-matrix.pdf", height = 3, width = 4)
